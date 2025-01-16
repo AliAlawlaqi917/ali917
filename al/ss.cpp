d4e13395010777312f1c693f229b97d978932518
AliAlawlaqi917 <aalialawlaqi.917@gmail.com>
2025-01-16 20:19:34 +0300

    mobil

---
+++ al/ss.cpp
@@ -0,0 +1,9 @@
+void main() {
+   string studentName = "Ali";
+   int studentID = 20231234;
+   string studentLevel = "Second Year";
+   
+   print('''ali$studentName
+   20231234$studentID
+   Second Year $studentLevel''');
+}
